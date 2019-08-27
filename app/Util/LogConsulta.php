67a97de56b79753322d1bdd9fdb378978c73b4e2
Márcio <marcio_santos9@estudante.sc.senai.br>
2019-08-26 21:42:41 -0300

    obter Ip ao acessar a pagina web

--- app/Util/LogConsulta.php
+++ app/Util/LogConsulta.php
@@ -18,9 +18,46 @@ class LogConsulta{
         }else{
             $data = 'Parâmetro inválido';
         }
-        file_put_contents($this->caminho.'/log_geral.txt', $data);
-        return $data;
         
+        if(file_exists($this->caminho.'/log_geral.txt')){
+            $dadosAtuais = $this->capturar();
+            $dadosAtuais .= "\n\n".$data;
+            $this->gravarArquivos($dadosAtuais);
+        } else {
+            $this->gravarArquivos($data);
         
-    }   
+        }
+        return $data; 
+               
+    }
+    
+    public function gravarArquivos($data){
+        
+    file_put_contents($this->caminho.'/log_geral.txt', $data);  
+  
+    }
+    
+    public function capturar(){
+        
+    $dados = file_get_contents($this->caminho.'/log_geral.txt');  
+    return $dados;
+    }
+    public function obterIp(){
+        
+
+    $pegar_ip = $_SERVER["REMOTE_ADDR"];
+    $ip_permitido = "ip_permitido";
+
+    if ($pegar_ip == $ip_permitido) 
+    {
+       echo 'Ip Permitido!';
+    } 
+    else 
+    { 
+        header("Location: url");
+    }
+  
+    }
+    
+    
 }
